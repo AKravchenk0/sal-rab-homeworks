93d6197487a899a4f18355c4661e6e6597b64469
Anna Kravchenko <anute664@gmail.com>
2021-07-31 18:00:43 +0300

    task 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -59,7 +59,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     
     freeShipping = shippingSum == 0;
     
-
+    
     // Конец решения задачи №2.3.
 
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
