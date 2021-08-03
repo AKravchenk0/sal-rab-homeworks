e9fd8ae11476d62fae95f4b57fc4e607c730cf38
Anna Kravchenko <anute664@gmail.com>
2021-08-03 19:51:14 +0300

    task final

--- task_final/solution.js
+++ task_final/solution.js
@@ -37,17 +37,18 @@ function sendRequest(name, phone, address, goods, sum) {
     let data = {order:{}, goods:[]};
 
     let countOfGoods = goods.length;
-
+    
     for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods = {order:{}, count:[]};
         data.goods.push(goods[i]);
         
     }
 
-    data.order.address = address;
+    data.order.address = "ул. "+address.street+", дом "+address.house+", "+address.entrance+" подъезд, "+address.floor+" этаж, кв "+address.flat;
     
     data.order.sum = sum;
 
-    data.client = name;
+    data.client = name+" "+phone;
 
     let jsonData = JSON.stringify({data});
 
